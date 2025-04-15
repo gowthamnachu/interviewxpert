cd0b6d6b588e62fbc9f6a34327aeb156637b8b7d
Gowtham Nachu <gowthamnachu545@gmail.com>
2025-04-15 20:16:54 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: '*',
+  origin: [process.env.VERCEL_URL || 'http://localhost:3000', 'https://interviewxpert.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
