c50f29c5badb23e46bb407becf705ccc7cbd2bf4
Gowtham Nachu <gowthamnachu545@gmail.com>
2025-04-15 20:24:40 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -366,9 +366,12 @@ app.use((err, req, res, next) => {
 });
 
 // Add catch-all route for React router
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
