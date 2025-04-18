488ec12eaec52108b5df3a86d54aa1b3d89dbafc
Gowtham Nachu <gowthamnachu545@gmail.com>
2025-04-18 13:28:55 +0530

    pushing4

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 const config = {
-  apiUrl: isDevelopment 
+  apiUrl: process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3001/api'
-    : '/.netlify/functions/api'
+    : 'https://interviewxpert.netlify.app/.netlify/functions/api'
 };
 
 export default config;
